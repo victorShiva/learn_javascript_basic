0bf62bcce8a556a3ee8fef331322e2ae7b6b5a9b
shiva gupta <shivagupta5607@gmail.com>
2024-05-05 07:36:13 +0530

    complete assignment

---
+++ 03_object/assignment.js
@@ -0,0 +1,28 @@
+// create a program tha generates a random number that should be between 1 to 6;
+Math.floor(Math.random() * 6) + 1;
+
+
+// create an object with name is car and pint the properties car:name , model , price.
+
+const car = {
+    name: "BMW",
+    model: "wt2000p",
+    color: "black"
+}
+
+console.log(car.name);
+
+
+// ceate person object name age and city
+//city => "New York"
+//add country , and set to "United States".
+
+const person = {
+    name: "Shiva",
+    age: 25,
+    city: "jaunpur"
+};
+
+person.city = "New York";
+person.county = "United States"
+console.log(person);
