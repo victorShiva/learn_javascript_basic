2960804960416ad03010917b0c7d7bd4550c11dc
shiva gupta <shivagupta5607@gmail.com>
2024-06-24 15:53:37 +0530

    calculate more number of values

---
+++ 20_interviews_questions/script.js
@@ -0,0 +1,33 @@
+// function calculate() {
+//     console.log(arguments);
+//     let result = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         result += arguments[i];
+//     }
+//     return result;
+// }
+// const result = calculate(23, 4, 7, 9, 98);
+// console.log(result);
+
+
+
+
+
+
+// function calculate(...array) {
+//     console.log(array);
+//     let result = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         result += array[i];
+//     }
+//     return result;
+// }
+// const result = calculate(23, 4, 7, 9, 98);
+// console.log(result);
+
+
+
+
+
+
+
