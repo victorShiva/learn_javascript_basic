4bf592a07904c3ed965aa9817ea144bad306acd2
shiva gupta <shivagupta5607@gmail.com>
2024-06-28 12:40:34 +0530

    this keyword is repeat learn

--- 06.1_this_keyword/function_this.js
+++ 06.1_this_keyword/function_this.js
@@ -1,4 +1,4 @@
-function get(){
+function get() {
     console.log(this);
 }
 // get();                                       // global object
@@ -31,9 +31,9 @@ function getInfo(obj) {
     console.log(this.name);
 }
 
-const person1 = {name:'shiva'};
-const person2 = {name:'Raghav'};
-const person3 = {name:'Dhirendra'};
+const person1 = { name: 'shiva' };
+const person2 = { name: 'Raghav' };
+const person3 = { name: 'Dhirendra' };
 
 // getInfo.call(person1);                       //shiva
 // getInfo.call(person2);                       //Raghav
@@ -59,16 +59,46 @@ const person3 = {name:'Dhirendra'};
 
 
 const personInfo = {
-    fav : 'Java',
-    job : 'youtube',
-    fun1 : function (){
+    fav: 'Java',
+    job: 'youtube',
+    fun1: function () {
         console.log(this);
-        const fun2= ()=>{
+        const fun2 = () => {
             console.log(this);
         }
         fun2();
     },
-    
+
 }
 
 personInfo.fun1();
+
+
+
+const student = {
+    name: "sanjeev",
+    age: 25,
+    prop: this,                     //global scope
+    getFun1: function () {          // object scope
+        console.log(this);
+        console.log(this.name);
+    },
+    getFun2: () => {
+        console.log(this);          // global scope
+        console.log(this.age);
+    },
+    getInfo1: function () {
+        setTimeout(() => {
+            console.log(this);          // object scope
+            console.log(this.name);
+        }, 1000);
+    },
+    getInfo2: function () {
+        setTimeout(function () {
+            console.log(this);              // global scope
+            console.log(this.age);
+        }, 1000);
+    }
+}
+
+console.log(object);
