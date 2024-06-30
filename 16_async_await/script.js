ac1137cb04c05b56ab8c4d7a97efdbfc7fa4544e
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-06-30 13:26:58 +0530

    async add new code resolve line no 275 - 322

--- 16_async_await/script.js
+++ 16_async_await/script.js
@@ -272,6 +272,58 @@
 
 
 
+function num() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let randNum = Math.floor(Math.random() * 10) + 1;
+            console.log(randNum);
+            resolve("Hello Good Morning");                   //print one time
+        }, 1000);
+    })
+};
+async function add() {
+    let first = await num();
+    console.log("first",first);                         //first Hello Good Morning
+    let second = await num();
+    console.log("second",second);                       //second Hello Good Morning
+    let last = await num();
+    console.log("last",last)                            //last Hello Good Morning
+}
+add()
+    .then(res => console.log("res",res))                //res undefined
+    .catch(err => console.log("err",err))
+
+
+
+
+
+
+
+function num() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let randNum = Math.floor(Math.random() * 10) + 1;
+            console.log(randNum);
+            resolve("Hello Good Morning");                   //print one time
+        }, 1000);
+    })
+};
+async function add() {
+    let first = await num();
+    console.log("first",first);                         //first Hello Good Morning
+    let second = await num();
+    console.log("second",second);                       //second Hello Good Morning
+    let last = await num();
+    console.log("last",last)                            //last Hello Good Morning
+    return last;
+}
+add()
+    .then(res => console.log("res",res))                //res Hello Good Morning
+    .catch(err => console.log("err",err))
+
+
+
+
 
 // let h1 = document.querySelector('h1');
 // function changeColor(color) {
